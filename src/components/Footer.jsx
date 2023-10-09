3940757d173f9f775049df03afa228917c96303f
Victoria <vmcnorrill@gmail.com>
2023-10-08 20:56:11 -0400

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,35 +1,35 @@
 
 const Footer = () => {
     <footer className="footer items-center p-4 bg-neutral text-neutral-content">
-    <aside className="items-center grid-flow-col">
-        <p>Copyright © 2023 - All right reserved</p>
-    </aside> 
-    <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-    <a href="https://github.com/victoriamcn" target="_blank" rel="noreferrer">
-            <span className="icon">
-              <i className="fab fa-github"></i>
-            </span>
-            <span className="social">GitHub</span>
-          </a>
-          <a href="https://www.linkedin.com/in/victoria-mcnorrill/" target="_blank" rel="noreferrer">
-            <span className="icon">
-              <i className="fab fa-linkedin"></i>
-            </span>
-            <span className="social">LinkedIn</span>
-          </a>
-          <a href="https://www.instagram.com/victoriamcnorrill/" target="_blank" rel="noreferrer">
-            <span className="icon">
-              <i className="fab fa-instagram"></i>
-            </span>
-            <span className="social">Instagram</span>
-          </a>
-          <a href="https://open.spotify.com/user/torilizabeth95" target="_blank" rel="noreferrer">
-            <span className="icon">
-              <i className="fab fa-spotify"></i>
-            </span>
-            <span className="social">Spotify</span>
-          </a>
-    </nav>
+        <aside className="items-center grid-flow-col">
+            <p>Copyright © 2023 - All right reserved</p>
+        </aside> 
+        <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+            <a href="https://github.com/victoriamcn" target="_blank" rel="noreferrer">
+                <span className="icon">
+                <i className="fab fa-github"></i>
+                </span>
+                <span className="social">GitHub</span>
+            </a>
+            <a href="https://www.linkedin.com/in/victoria-mcnorrill/" target="_blank" rel="noreferrer">
+                <span className="icon">
+                <i className="fab fa-linkedin"></i>
+                </span>
+                <span className="social">LinkedIn</span>
+            </a>
+            <a href="https://www.instagram.com/victoriamcnorrill/" target="_blank" rel="noreferrer">
+                <span className="icon">
+                <i className="fab fa-instagram"></i>
+                </span>
+                <span className="social">Instagram</span>
+            </a>
+            <a href="https://open.spotify.com/user/torilizabeth95" target="_blank" rel="noreferrer">
+                <span className="icon">
+                <i className="fab fa-spotify"></i>
+                </span>
+                <span className="social">Spotify</span>
+            </a>
+        </nav>
     </footer>
 }
 
