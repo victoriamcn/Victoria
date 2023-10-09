ba3cfa44494176eb4f014206e16a0b388f27c336
Victoria <vmcnorrill@gmail.com>
2023-10-08 20:12:35 -0400

    fixed tailwind, installing DaisyUI and setting up portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+/*eslint-env node*/
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+        './src/pages/*.{js,ts,jsx,tsx}',
+        './src/components/**/*.{js,ts,jsx,tsx}'
+    ],
     theme: {
       extend: {},
     },
-    plugins: [],
+    plugins: [require('daisyui')],
   }
