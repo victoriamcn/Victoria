6078102157c5698ac8483568f95ec18bb0895473
Victoria <vmcnorrill@gmail.com>
2023-10-08 20:26:18 -0400

    added customized daisyui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,22 @@ module.exports = {
     theme: {
       extend: {},
     },
+    daisyui: {
+        themes: [
+          {
+            mytheme: {
+            "primary": "#C53C80",
+            "secondary": "#EE583F",
+            "accent": "#0464D4",
+            "neutral": "#1E1B15",  
+            "base-100": "#F8F8F7",  
+            "info": "#9CA3F9",   
+            "success": "#065f46",  
+            "warning": "#fb923c",
+            "error": "#9f1239",
+            },
+          },
+        ],
+      },
     plugins: [require('daisyui')],
   }
