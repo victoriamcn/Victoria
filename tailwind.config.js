57a74d8405feb58bd42c0d65538e3ddeec3c7296
Victoria <vmcnorrill@gmail.com>
2023-10-16 19:09:18 -0400

    attempt to connect tailwind to screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 /*eslint-env node*/
 module.exports = {
-    content: [
-        './index.html',
-        './src/**/*.{js,ts,jsx,tsx}',
-        './src/pages/*.{js,ts,jsx,tsx}',
-        './src/components/**/*.{js,ts,jsx,tsx}'
-    ],
-    theme: {
-      extend: {},
-    },
+  content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}',
+      './src/pages/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['Fjalla One', 'Bebas Neue', 'ui-sans-serif', 'system-ui'],
+      'serif': [],
+      'mono': [],
+      'display': ['Modak','Oi', 'Ultra', 'ui-sans-serif', 'system-ui'],
+      'body': ['Heebo', 'Roboto', 'Helvetica', 'Open Sans', 'ui-sans-serif', 'system-ui'],
+    }
+  },
     daisyui: {
         themes: [
           {
